242622dc2e33d6353e8899b0a191c3de1d567ac6
Juan Sebastian Vanegas <sebastian44van@gmail.com>
2017-11-09 21:39:40 -0500

    Introduccion a Entity Framework

---
+++ Tienda/Tienda/categorias.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="categorias.aspx.cs" Inherits="Tienda.categorias" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ListBox runat="server" ID="lstCategorias">
+            </asp:ListBox>
+        </div>
+        <div>
+
+            <asp:TextBox runat="server" ID="txtNombre" placeholder="Nombre." />
+            <asp:TextBox runat="server" ID="txtImagen" placeholder="URL Imagen" />
+            <asp:Button Text="Enviar" runat="server" ID="btnEnviar" OnClick="btnEnviar_Click" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Tienda/Tienda/categorias.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tienda
+{
+    public partial class categorias : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ConsultarCategorias();
+        }
+
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+            using (var cnn = new TiendaEntities())
+            {
+                var nuevaCategoria = new Categoria() {
+                    NombreCategoria = txtNombre.Text,
+                    Imagen = txtImagen.Text
+                };
+                cnn.Categoria.Add(nuevaCategoria);
+                cnn.SaveChanges();
+                ConsultarCategorias();
+            }
+        }
+
+        private void ConsultarCategorias() {
+            using (var cnn = new TiendaEntities())
+            {
+                lstCategorias.DataSource = cnn.Categoria.ToList();
+                lstCategorias.DataTextField = "NombreCategoria"; // -> Lo que se muestra
+                lstCategorias.DataValueField = "IdCategoria"; // -> Lo que vale cada opcion
+                lstCategorias.DataBind();
+            } // --> Cierra la conexion a la BD
+        }
+    }
+}
